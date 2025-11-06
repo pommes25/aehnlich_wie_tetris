e48b94f514553cef4c895d68f23b08fe7bbc3b01
pommes25 <h.rabauer@gmail.com>
2025-11-06 17:46:00 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,14 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        injectOptions: {
-          tags: [
-            {
-              tag: 'script',
-              attrs: {
-                src: '/src/main.js',
-                type: 'module',
-              },
-            },
-            {
-              tag: 'link',
-              attrs: {
-                rel: 'stylesheet',
-                href: '/src/styles/main.css',
-              },
-            },
-          ],
-        },
-      },
-    }),
-  ],
-  server: {
-    open: true,
-  },
+  // Ensure Vite/Rollup uses the HTML file in the `public/` folder as the entry
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html',
+    },
+  },
+  server: {
+    open: true,
   },
 });
